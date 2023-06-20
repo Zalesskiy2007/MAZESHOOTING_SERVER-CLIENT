e9a73368d6c508c2cab5e29f5a47bd982a14b016
Michael <75903594+Zalesskiy2007@users.noreply.github.com>
2023-06-20 16:27:52 +0300

    Add files via upload
    Made some style.

--- client/client.js
+++ client/client.js
@@ -3,6 +3,29 @@ import { main } from "./src/main.js";
 
 const socket = io();
 
+function addInfoBlock() {
+  let block = document.getElementById("wrap");
+  block.innerHTML = "";
+
+  if (window.otherPlayers !== null) {
+  block.insertAdjacentHTML("beforeend", `<div class="person" style="background-color: black;">
+                                            <div class="pers-color"></div>
+                                            <div class="pers-name">${window.player.name}</div>
+                                            <div class="pers-stat">${window.player.health}/100</div>
+                                         </div>`);
+  }
+  
+  if (window.otherPlayers !== null) {
+    for (let i = 0; i < window.otherPlayers.length; i++) {
+      block.insertAdjacentHTML("beforeend", `<div class="person">
+                                              <div class="pers-color"></div>
+                                              <div class="pers-name">${window.otherPlayers[i].name}</div>
+                                              <div class="pers-stat">${window.otherPlayers[i].health}/100</div>
+                                          </div>`);
+    }
+  }
+}
+
 async function mainClient() {
   // client-side
   socket.on("connect", () => {
@@ -18,12 +41,13 @@ async function mainClient() {
         window.otherPlayers.push(JSON.parse(tmpPlayers[i]));
       }
     }
-
+    addInfoBlock();
     console.log("Other: " + msg);
   });
 
   socket.on("MFS:Get_Player", function(msg) {
     window.player = JSON.parse(msg);
+    addInfoBlock();
     console.log("Player: " + msg);
   });
 
@@ -36,8 +60,18 @@ async function mainClient() {
     if (window.player === null) {
       let playerName = document.getElementById("playerName").value;
       let playerRoom = document.getElementById("room").value;
+      let title = document.getElementById("roomShow");
 
-      socket.emit("MTS:Player_Settings", [playerName, playerRoom].join('|'));
+      if (playerName !== "" && playerRoom !== "") {
+        socket.emit("MTS:Player_Settings", [playerName, playerRoom].join('|'));
+        title.innerText = `Your room is '${playerRoom}'`;
+        title.style.color = "aliceblue";
+        title.style.fontStyle = "normal";
+      } else {
+        title.innerText = `invalid room or player name`;
+        title.style.color = "red";
+        title.style.fontStyle = "italic";
+      }
     }
   }
 
